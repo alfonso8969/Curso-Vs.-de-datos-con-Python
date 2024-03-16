737287a7ff260dbefda27cb7de52c5019309f2cd
Alfonso J. González <alfonso8969@hotmail.es>
2024-03-16 19:57:18 +0100

    Se crean carpetas por sección.Empieza Seccion 5

--- section 4/areas_graf.py
+++ section 4/areas_graf.py
@@ -1,8 +1,45 @@
 import pandas as pd
 # import matplotlib.pyplot as plt
 
-df = pd.read_csv("../../data/tienda_ventas.csv", index_col='id')
+df = pd.read_csv("../../../data/tienda_ventas.csv", index_col='id')
 df
 
 df["mes"] = df["date"].apply(lambda x: x[:7])
 df
+
+sell_by_month = df.groupby('mes').agg({
+    'sales': 'sum'
+})
+
+sell_by_month.plot.line(
+    y='sales',
+    style='.-',
+    linestyle='dotted'
+)
+
+sell_by_month.plot.area(
+    y='sales'
+)
+
+
+df['year'] = df['date'].apply(lambda x: x[0:4])
+df
+
+df_2015 = df[
+    (df['year'] == '2015') & 
+    (df['family'].isin(['SEAFOOD', 'MEATS', 'DAIRY']))
+]
+df_2015
+
+df_2015_grouped = df_2015.groupby(['mes', 'family']).agg({
+    'sales': 'sum'
+})
+df_2015_grouped
+
+df_2015_pivot = df_2015_grouped.reset_index()
+df_2015_pivot = df_2015_pivot.pivot(index='mes', columns='family', values='sales')
+df_2015_pivot.plot.line()
+
+df_2015_pivot.plot.area(
+    stacked=False
+)
